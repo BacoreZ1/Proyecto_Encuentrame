53b70d7a634173e599a05c694a417f25b5fc1bc1
bacolinx <bacolinx@gmail.com>
2018-08-07 15:50:51 -0500

    Agregue Adaptador_Categoria y Mapa

--- app/src/main/java/encuentrame/ec/proyecto_encuentrame/Categoria_Adaptador.kt
+++ app/src/main/java/encuentrame/ec/proyecto_encuentrame/Categoria_Adaptador.kt
@@ -1,23 +1,41 @@
 package encuentrame.ec.proyecto_encuentrame
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_categoria.view.*
 
-class Categoria_Adaptador(): RecyclerView.Adapter<Categoria_Adaptador.ViewHolder>(){
+class Categoria_Adaptador(var categorias:ArrayList<String>): RecyclerView.Adapter<Categoria_Adaptador.ViewHolder>(){
+
+
+    //Inflar nuestra vista(vamos a declar el layaout q vamos utilizar)
+    //dibuja
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_categoria, parent, false)
+        return ViewHolder(view)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getItemCount(): Int {
+    //public suma ():int publuic int
+
+    //devuelve cuantos elementos devuelve el adaptador
+    //numero de elementos que tiene el adaptador*
+        override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return  categorias.size
     }
 
+    //setea los valores en la vista(o visualmente)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var categoria= categorias.get(position)
+
+        //seteo la categoria
+        holder.view.tv_categoria.text = categoria
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
+//Gestionar los elemento ( recibimos las refencias)
     class ViewHolder(var view:View):RecyclerView.ViewHolder(view){
 
     }
