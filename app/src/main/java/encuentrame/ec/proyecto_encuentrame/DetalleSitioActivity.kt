5dd13625aa695118f83e9f3d26fdcf0db628c799
bacolinx <bacolinx@gmail.com>
2018-08-27 17:11:45 -0500

    Finalizacion del Detalle del Perfil del Sitio(ultimo layout)

--- app/src/main/java/encuentrame/ec/proyecto_encuentrame/DetalleSitioActivity.kt
+++ app/src/main/java/encuentrame/ec/proyecto_encuentrame/DetalleSitioActivity.kt
@@ -2,11 +2,43 @@ package encuentrame.ec.proyecto_encuentrame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detalle_sitio.*
+
 
 class DetalleSitioActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_sitio)
+
+        var sitio= intent.getSerializableExtra("sitio") as Sitios
+
+        //ya se tiene informacion y ahora se seta la activitada
+
+        //setear las variables
+
+        tv_descripcion.text = sitio.descripcion
+        supportActionBar!!.title= sitio.nombre
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        Glide.with(this)
+                .load(sitio.foto)
+                .into(img_foto)
+
+
+    }
+//
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        //when swuit como n java
+        when(item!!.itemId){
+            //capturar evento de boton hacina atras
+            android.R.id.home ->{
+                //terminar evento
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
